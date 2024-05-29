bab9fa3ca85d70a3f93e56e13929a8d503957385
DIR\marina.carracedo <marinacarracedo14@gmail.com>
2024-05-29 09:50:30 -0300

    Agrego relacion n-n de hospital y personas

--- src/main/java/com/example/hospital/model/Hospital.java
+++ src/main/java/com/example/hospital/model/Hospital.java
@@ -5,6 +5,9 @@ import java.util.HashSet;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
@@ -18,4 +21,19 @@ public class Hospital {
 	@OneToMany(mappedBy = "hospital", cascade = CascadeType.ALL, orphanRemoval = true)
 	private HashSet<Department> departments;
 	
+	@ManyToMany
+    @JoinTable(
+        name = "hospital_staff",
+        joinColumns = @JoinColumn(name = "hospital_id"),
+        inverseJoinColumns = @JoinColumn(name = "staff_id")
+    )
+    private HashSet<Staff> staff;
+	
+	@ManyToMany
+    @JoinTable(
+        name = "hospital_patient",
+        joinColumns = @JoinColumn(name = "hospital_id"),
+        inverseJoinColumns = @JoinColumn(name = "patient_id")
+    )
+    private HashSet<Patient> patient;
 }
